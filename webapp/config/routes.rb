9057696c221d7870f01f6474d21782029f881afc
jack <jackconradsen@gmail.com>
2021-04-05 23:34:53 +0200

    adding

---
+++ webapp/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  as :user do
+    # Define routes for User within this block.
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*other', to: 'static#index'
+end
